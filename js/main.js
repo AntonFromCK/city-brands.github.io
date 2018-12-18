3c9f0b3ac52422687b1ca80e49cdb7da61f84eb5
Anton Nechuporenko <antonnechuporenko@gmail.com>
2018-12-19 00:34:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider-shop').slick({
+        adaptiveHeight:true,
+        slidesToShow: 1,
+        autoplay:true,
+        autoplaySpeed:2000
+    });
+});
+
