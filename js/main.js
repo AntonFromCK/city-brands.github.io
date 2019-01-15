132a47e8f24a03e88fd68004154319b197aea1ea
Anton <antonnechuporenko@gmail.com>
2019-01-15 22:01:56 +0200

    update something

--- js/main.js
+++ js/main.js
@@ -55,41 +55,55 @@ function scaleBannerVideoSize(element) {
 
     });
 }
+$( document ).ready(function() {
 
-$('.present-slider').slick({
-    adaptiveHeight:true,
-    slidesToShow: 5,
-    autoplay:true,
-    autoplaySpeed:2000,
-    responsive: [
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
+    $('.present-slider').slick({
+        adaptiveHeight:true,
+        slidesToShow: 5,
+        autoplay:true,
+        autoplaySpeed:2000,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
             }
+        ]
+    });
+    $('#mob-nav-icon').click(function(){
+        $(this).toggleClass('open');
+        if($(this).hasClass('open')){
+            $('.nav-mob-wrapper').css("display", "block");
+        } else {
+            $('.nav-mob-wrapper').css("display", "none");
         }
-    ]
+    });
+
+    $('form').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function () {
+                console.log("oll ok");
+            }
+        })
+    })
 });
-$('#mob-nav-icon').click(function(){
-    $(this).toggleClass('open');
-    if($(this).hasClass('open')){
-        $('.nav-mob-wrapper').css("display", "block");
-    } else {
-        $('.nav-mob-wrapper').css("display", "none");
+$(window).resize(function(){
+    $.fn.swap = function (elem) {
+        elem = elem.jquery ? elem : $(elem);
+        return this.each(function () {
+            $(document.createTextNode('')).insertBefore(this).before(elem.before(this)).remove();
+        });
+    };
+    if ($(window).width() <= 767) {
+        $('#present').swap('.test')
     }
 });
 
-$('form').submit(function (event) {
-    event.preventDefault();
-    $.ajax({
-        type: $(this).attr('method'),
-        url: $(this).attr('action'),
-        data: new FormData(this),
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function () {
-            console.log("oll ok");
-        }
-    })
-})
